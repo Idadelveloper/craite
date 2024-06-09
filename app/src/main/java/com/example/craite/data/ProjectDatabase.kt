47a0250b86bc7e613ab93ec040dfebb1dc1b1d3e
Ida Delphine <idadelm@gmail.com>
2024-06-09 22:47:30 +0100

    clear project database code

--- app/src/main/java/com/example/craite/data/ProjectDatabase.kt
+++ app/src/main/java/com/example/craite/data/ProjectDatabase.kt
@@ -7,26 +7,3 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.craite.utils.Converters
 
-@Database(entities = [Project::class], version = 1, exportSchema = false)
-@TypeConverters(Converters::class)
-abstract class ProjectDatabase : RoomDatabase() {
-
-    abstract fun projectDao(): ProjectDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: ProjectDatabase? = null
-
-        fun getDatabase(context: Context): ProjectDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ProjectDatabase::class.java,
-                    "project_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
-}
