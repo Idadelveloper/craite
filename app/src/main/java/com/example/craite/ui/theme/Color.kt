a5e4b304e9c8993741a8dc758e3225c17ea28354
nuilewis <lewisnuikweh@gmail.com>
2024-07-10 12:27:51 +0100

    ui: setting up colors and theming

--- app/src/main/java/com/example/craite/ui/theme/Color.kt
+++ app/src/main/java/com/example/craite/ui/theme/Color.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.example.craite.ui.theme
 
 import androidx.compose.ui.graphics.Color
@@ -9,3 +11,137 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+class AppColor {
+
+
+
+
+    ///-----Purple----///
+    val purpleSeed = Color(0xFF793FFA)
+    val purple0 = Color(0xFF000000)
+    val purple05 = Color(0xFF160042)
+    val purple10 = Color(0xFF22005D)
+    val purple15 = Color(0xFF2D0077)
+    val purple20 = Color(0xFF3A0093)
+    val purple25 = Color(0xFF4600AF)
+    val purple30 = Color(0xFF5300CC)
+    val purple35 = Color(0xFF6016E1)
+    val purple40 = Color(0xFF6C2DED)
+    val purple50 = Color(0xFF8553FF)
+    val purple60 = Color(0xFF9D79FF)
+    val purple70 = Color(0xFFB69BFF)
+    val purple80 = Color(0xFFCFBDFF)
+    val purple90 = Color(0xFFE9DDFF)
+    val purple95 = Color(0xFFF6EEFF)
+    val purple98 = Color(0xFFFDF7FF)
+    val purple99 = Color(0xFFFFFBFF)
+
+
+    ///-----Green----///
+    val greenSeed = Color(0xFFDAF535)
+    val green0 = Color(0xFF000000)
+    val green05 = Color(0xFF0F1300)
+    val green10 = Color(0xFF191E00)
+    val green15 = Color(0xFF222900)
+    val green20 = Color(0xFF2C3400)
+    val green25 = Color(0xFF373F00)
+    val green30 = Color(0xFF414B00)
+    val green35 = Color(0xFF4C5800)
+    val green40 = Color(0xFF586400)
+    val green50 = Color(0xFF6F7F00)
+    val green60 = Color(0xFF879A00)
+    val green70 = Color(0xFFA0B600)
+    val green80 = Color(0xFFB9D300)
+    val green90 = Color(0xFFDCEA8D)
+    val green95 = Color(0xFFEBF99A)
+    val green98 = Color(0xFFF6FFBE)
+    val green99 = Color(0xFFFCFFDC)
+
+
+    ///-----Orange----///
+    val orangeSeed = Color(0xFFFFB84E)
+    val orange0 = Color(0xFF000000)
+    val orange05 = Color(0xFF1B0E00)
+    val orange10 = Color(0xFF291800)
+    val orange15 = Color(0xFF372100)
+    val orange20 = Color(0xFF452B00)
+    val orange25 = Color(0xFF543500)
+    val orange30 = Color(0xFF633F00)
+    val orange35 = Color(0xFF724A00)
+    val orange40 = Color(0xFF825500)
+    val orange50 = Color(0xFFA36B00)
+    val orange60 = Color(0xFFC2841B)
+    val orange70 = Color(0xFFE19E36)
+    val orange80 = Color(0xFFFFB952)
+    val orange90 = Color(0xFFFFDDB4)
+    val orange95 = Color(0xFFFFEEDC)
+    val orange98 = Color(0xFFFFF8F4)
+    val orange99 = Color(0xFFFFFBFF)
+
+
+    ///-----Blue----///
+    val blueSeed = Color(0xFF0F99FD)
+    val blue0 = Color(0xFF000000)
+    val blue05 = Color(0xFF001225)
+    val blue10 = Color(0xFF001D36)
+    val blue15 = Color(0xFF002747)
+    val blue20 = Color(0xFF003259)
+    val blue25 = Color(0xFF003D6B)
+    val blue30 = Color(0xFF00497D)
+    val blue35 = Color(0xFF005591)
+    val blue40 = Color(0xFF0061A4)
+    val blue50 = Color(0xFF007ACD)
+    val blue60 = Color(0xFF0095F8)
+    val blue70 = Color(0xFF64AFFF)
+    val blue80 = Color(0xFF9FCAFF)
+    val blue90 = Color(0xFFD1E4FF)
+    val blue95 = Color(0xFFEAF1FF)
+    val blue98 = Color(0xFFF8F9FF)
+    val blue99 = Color(0xFFFDFCFF)
+
+    ///-----Red----///
+    val redSeed = Color(0xFFFF5449)
+    val red0 = Color(0xFF000000)
+    val red05 = Color(0xFF2D0001)
+    val red10 = Color(0xFF410002)
+    val red15 = Color(0xFF540003)
+    val red20 = Color(0xFF690005)
+    val red25 = Color(0xFF7E0007)
+    val red30 = Color(0xFF93000A)
+    val red35 = Color(0xFFA60F13)
+    val red40 = Color(0xFFB81F1E)
+    val red50 = Color(0xFFDB3A33)
+    val red60 = Color(0xFFFF5449)
+    val red70 = Color(0xFFFF897D)
+    val red80 = Color(0xFFFFB4AB)
+    val red90 = Color(0xFFFFDAD6)
+    val red95 = Color(0xFFFFEDEA)
+    val red98 = Color(0xFFFFF8F7)
+    val red99 = Color(0xFFFFFBFF)
+
+
+    ///-----Neutral----///
+    val neutral0 = Color(0xFF000000)
+    val neutral05 = Color(0xFF121014)
+    val neutral10 = Color(0xFF1C1B1E)
+    val neutral15 = Color(0xFF272529)
+    val neutral20 = Color(0xFF313033)
+    val neutral25 = Color(0xFF3D3B3E)
+    val neutral30 = Color(0xFF48464A)
+    val neutral35 = Color(0xFF545156)
+    val neutral40 = Color(0xFF605D62)
+    val neutral50 = Color(0xFF79767A)
+    val neutral60 = Color(0xFF938F94)
+    val neutral70 = Color(0xFFAEAAAE)
+    val neutral80 = Color(0xFFCAC5CA)
+    val neutral90 = Color(0xFFE6E1E6)
+    val neutral95 = Color(0xFFF4EFF4)
+    val neutral98 = Color(0xFFFDF8FD)
+    val neutral99 = Color(0xFFFFFBFF)
+
+
+    ///Black and white
+    val black =  Color(0xFF121014)
+    val white = Color(0xFFFFFFFF)
+}
