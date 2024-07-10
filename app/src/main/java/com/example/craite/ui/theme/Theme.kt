02c9457c5f8bbb2fbd755d01f6288ca7a90eaa85
nuilewis <lewisnuikweh@gmail.com>
2024-07-10 12:27:51 +0100

    ui: setting up colors and theming

--- app/src/main/java/com/example/craite/ui/theme/Theme.kt
+++ app/src/main/java/com/example/craite/ui/theme/Theme.kt
@@ -11,27 +11,56 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+class AppColorScheme{
+    private val appColors = AppColor()
+    val darkColorScheme = darkColorScheme(
+        primary = appColors.greenSeed,
+        onPrimary = appColors.black,
+        primaryContainer = appColors.green20,
+        onPrimaryContainer = appColors.green90,
+
+        secondary = appColors.purple70,
+        onSecondary = appColors.black,
+        secondaryContainer = appColors.purple20,
+        onSecondaryContainer = appColors.purple90,
+
+        tertiary = appColors.blueSeed,
+        onTertiary = appColors.black,
+        tertiaryContainer = appColors.blue20,
+        onTertiaryContainer = appColors.blue90,
+
+
+        background = appColors.black,
+        onBackground = appColors.white,
+        surface = appColors.neutral15,
+        onSurface = appColors.white,
+
+        error = appColors.redSeed,
+        onError = appColors.white,
+        errorContainer = appColors.red20,
+        onErrorContainer = appColors.white,
+    )
+
+    val lightColorScheme = lightColorScheme(
+        primary = Purple40,
+        secondary = PurpleGrey40,
+        tertiary = Pink40
+
+        /* Other default colors to override
+        background = Color(0xFFFFFBFE),
+        surface = Color(0xFFFFFBFE),
+        onPrimary = Color.White,
+        onSecondary = Color.White,
+        onTertiary = Color.White,
+        onBackground = Color(0xFF1C1B1F),
+        onSurface = Color(0xFF1C1B1F),
+        */
+    )
+}
+
+
 
 @Composable
 fun CraiteTheme(
@@ -40,19 +69,19 @@ fun CraiteTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+//    val colorScheme = when {
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+//
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+//    }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+        colorScheme = AppColorScheme().darkColorScheme,
+        typography = AppTypography().typography,
         content = content
     )
 }
