978b07f2c1d7eb631c249753920abdebfa93a499
nuilewis <lewisnuikweh@gmail.com>
2024-07-13 14:23:49 +0100

    ui: built project creations screen

--- app/src/main/java/com/example/craite/ui/screens/project/CreateProjectScreen.kt
+++ app/src/main/java/com/example/craite/ui/screens/project/CreateProjectScreen.kt
@@ -7,16 +7,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Create
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -27,6 +32,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +40,7 @@ import androidx.compose.ui.unit.dp
 import com.example.craite.R
 import com.example.craite.ui.screens.composables.CraiteTextField
 import com.example.craite.ui.screens.composables.GradientImageBackground
+import com.example.craite.ui.screens.project.composables.FootageThumbnail
 
 
 @Preview(showBackground = true)
@@ -41,32 +48,56 @@ import com.example.craite.ui.screens.composables.GradientImageBackground
 fun CreateProjectScreen(modifier: Modifier = Modifier) {
     Scaffold {
         innerPadding ->
-        Box {
+        Box() {
             GradientImageBackground(
                 painter = painterResource(id = R.drawable.surfing),
                 contentDescription = "Gradient overlay on surfer",
                 modifier = Modifier.fillMaxHeight(.3f)
             )
-            Column(modifier = Modifier
-                .padding(innerPadding)
-                .padding(horizontal = 16.dp)
-                .fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
                 Spacer(modifier = modifier.fillMaxHeight(.2f))
-                Text(text = "Create Project", style = MaterialTheme.typography.headlineSmall, onTextLayout = null)
+                Text(
+                    text = "Create Project",
+                    style = MaterialTheme.typography.headlineSmall,
+                    onTextLayout = null
+                )
                 Spacer(modifier = Modifier.height(24.dp))
 
                 CraiteTextField(label = "Project Name", hintText = " This is a hint Text")
-                CraiteTextField(label = "Prompt", hintText = "Prompt", minLines = 6)
-                Spacer(modifier = Modifier.weight(1f))
-                Button(onClick = { /*TODO*/ }, modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp)) {
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "Footage", style = MaterialTheme.typography.titleMedium)
+                Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+                    FootageThumbnail()
+                    FootageThumbnail()
+                    FootageThumbnail()
+                }
+
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth().height(72.dp), shape = RoundedCornerShape(20.dp), colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)) {
                     Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
-Spacer(modifier = Modifier.width(8.dp))
-                        Text(text = "Create Project")
+                        Icon(imageVector = Icons.Rounded.Add, contentDescription = null, tint = MaterialTheme.colorScheme.primary)
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "Import Footage", style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary))
                     }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                CraiteTextField(label = "Prompt", hintText = "Prompt", minLines = 6)
+                
+
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    onClick = { /*TODO*/ }, modifier = Modifier
+                        .fillMaxWidth()
+                        .height(52.dp)
+                ) {
+
+                        Text(text = "Create Project")
+
                 }
                 Spacer(modifier = Modifier.height(48.dp))
             }
---
+++ app/src/main/java/com/example/craite/ui/screens/project/composables/FootageThumbnail.kt
@@ -0,0 +1,36 @@
+package com.example.craite.ui.screens.project.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.craite.R
+
+@Preview()
+@Composable
+fun FootageThumbnail(modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.spacedBy(12.dp), modifier = Modifier.width(100.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.surfing),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(16.dp))
+                .aspectRatio(11 / 16f)
+        )
+        Text(text = "Video Title", style = MaterialTheme.typography.bodySmall)
+
+
+    }
+}
