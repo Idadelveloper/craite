6abc91777401ed0c6a6d53954a9e14422e46a353
nuilewis <lewisnuikweh@gmail.com>
2024-07-16 14:47:19 +0100

    ui: text field updates

--- app/src/main/java/com/example/craite/ui/screens/composables/CraiteTextField.kt
+++ app/src/main/java/com/example/craite/ui/screens/composables/CraiteTextField.kt
@@ -2,14 +2,11 @@ package com.example.craite.ui.screens.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -17,46 +14,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import androidx.compose.ui.unit.min
 
 @Composable
-fun CraiteTextField(modifier: Modifier = Modifier, label: String, hintText: String, minLines: Int = 1 ) {
-var value by remember {
-    mutableStateOf(TextFieldValue(""))
-}
+fun CraiteTextField(
+    modifier: Modifier = Modifier,
+    label: String,
+    hintText: String,
+    minLines: Int = 1,
+    onValueChanged: (TextFieldValue) -> Unit
+) {
+    val value by remember {
+        mutableStateOf(TextFieldValue())
+    }
 
-    var borderRadius= 28.0
-    if (minLines ==1){
+    var borderRadius = 28.0
+    if (minLines == 1) {
         borderRadius = 28.0
     } else {
         borderRadius = 20.0
     }
 ///Todo: Flesh out the implementation of this text field
     Column(
-        modifier = Modifier.fillMaxWidth().then(modifier),
+        modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(text = label, style = MaterialTheme.typography.bodyMedium )
+        Text(text = label, style = MaterialTheme.typography.bodyMedium)
 
         TextField(
             value = value,
-            onValueChange = { newValue:TextFieldValue ->
-
+            onValueChange = {
+                onValueChanged(it)
             },
 
-           placeholder = {Text(text = hintText)},
+            placeholder = { Text(text = hintText) },
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(0.dp)
-
-           ,
+                .padding(0.dp),
             shape = RoundedCornerShape(borderRadius.dp),
             singleLine = minLines == 1,
             minLines = minLines,
--- app/src/main/java/com/example/craite/ui/screens/project/CreateProjectScreen.kt
+++ app/src/main/java/com/example/craite/ui/screens/project/CreateProjectScreen.kt
@@ -8,33 +8,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.Create
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.craite.R
@@ -46,9 +39,16 @@ import com.example.craite.ui.screens.project.composables.FootageThumbnail
 @Preview(showBackground = true)
 @Composable
 fun CreateProjectScreen(modifier: Modifier = Modifier) {
-    Scaffold {
-        innerPadding ->
-        Box() {
+
+    val projectNameValue = remember {
+        mutableStateOf(TextFieldValue() )
+    }
+
+    val promptValue = remember { mutableStateOf(TextFieldValue())}
+    Scaffold(
+         modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Box {
             GradientImageBackground(
                 painter = painterResource(id = R.drawable.surfing),
                 contentDescription = "Gradient overlay on surfer",
@@ -69,7 +69,11 @@ fun CreateProjectScreen(modifier: Modifier = Modifier) {
                 )
                 Spacer(modifier = Modifier.height(24.dp))
 
-                CraiteTextField(label = "Project Name", hintText = " This is a hint Text")
+                CraiteTextField(
+                    label = "Project Name",
+                    hintText = " This is a hint Text",
+                    onValueChanged = {projectNameValue.value = it}
+                    )
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(text = "Footage", style = MaterialTheme.typography.titleMedium)
                 Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
@@ -78,16 +82,37 @@ fun CreateProjectScreen(modifier: Modifier = Modifier) {
                     FootageThumbnail()
                 }
 
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth().height(72.dp), shape = RoundedCornerShape(20.dp), colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(72.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)
+                ) {
                     Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(imageVector = Icons.Rounded.Add, contentDescription = null, tint = MaterialTheme.colorScheme.primary)
+                        Icon(
+                            imageVector = Icons.Rounded.Add,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.primary
+                        )
                         Spacer(modifier = Modifier.width(8.dp))
-                        Text(text = "Import Footage", style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary))
+                        Text(
+                            text = "Import Footage",
+                            style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.primary)
+                        )
                     }
                 }
                 Spacer(modifier = Modifier.height(8.dp))
-                CraiteTextField(label = "Prompt", hintText = "Prompt", minLines = 6)
-                
+                CraiteTextField(
+                    label = "Prompt",
+                    hintText = "Prompt",
+                    minLines = 6,
+                    onValueChanged = {
+                        promptValue.value = it
+                    }
+                )
+
 
                 Spacer(modifier = Modifier.weight(1f))
                 Button(
@@ -96,7 +121,7 @@ fun CreateProjectScreen(modifier: Modifier = Modifier) {
                         .height(52.dp)
                 ) {
 
-                        Text(text = "Create Project")
+                    Text(text = "Create Project")
 
                 }
                 Spacer(modifier = Modifier.height(48.dp))
