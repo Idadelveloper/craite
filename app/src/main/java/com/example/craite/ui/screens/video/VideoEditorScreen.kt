120c5b84a7371fa8e9d3bfc44df33beeb965d966
Ida Delphine <idadelm@gmail.com>
2024-07-24 11:45:04 +0100

    add video editor view model

--- app/src/main/java/com/example/craite/ui/screens/video/VideoEditorScreen.kt
+++ app/src/main/java/com/example/craite/ui/screens/video/VideoEditorScreen.kt
@@ -1,5 +1,7 @@
 package com.example.craite.ui.screens.video
 
+import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -21,18 +23,27 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.media3.common.MediaItem
 import androidx.media3.common.Timeline
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.navigation.NavController
+import com.example.craite.VideoEditViewModel
 import com.example.craite.data.models.Project
 import com.example.craite.data.models.ProjectDatabase
+import com.example.craite.generateFakeEditSettings
 import com.example.craite.ui.screens.video.composables.PlaybackControls
 import com.example.craite.ui.screens.video.composables.Timeline
 import com.example.craite.ui.screens.video.composables.VideoPreview
 import com.google.firebase.auth.FirebaseUser
+import java.io.File
 
 //@Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -42,6 +53,20 @@ fun VideoEditorScreen(
     user: FirebaseUser?,
     projectDatabase: ProjectDatabase
 ) {
+    val context = LocalContext.current
+    val exoPlayer = remember { ExoPlayer.Builder(context).build() }
+    val viewModel = remember { VideoEditorViewModel(generateFakeEditSettings(project.media.size)) }
+    val currentMediaIndex by viewModel.currentMediaItemIndex.collectAsState()
+    val mediaUris = project.media.map { Uri.fromFile(File(it)) }
+    val mediaItemMap = mediaUris.indices.associateWith { MediaItem.fromUri(mediaUris[it]) }
+    val editSettings by viewModel.uiState.collectAsState()
+    val showProgressDialog by viewModel.showProgressDialog.collectAsState()
+    val downloadButtonEnabled by viewModel.downloadButtonEnabled.collectAsState()
+
+    Log.d("VideoEditScreen", "MediaItemMap: ${project.mediaNames.entries}")
+    Log.d("VideoEditScreen", "MediaItems: ${project.media}")
+    Log.d("VideoEditScreen", "PromptId: ${project.promptId}")
+
     Scaffold(
          modifier = Modifier.fillMaxSize(),
     ) {
