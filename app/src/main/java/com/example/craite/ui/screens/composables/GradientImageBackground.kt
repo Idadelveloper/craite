5b000eda7bae817895c5472edf1342fc7013e3a7
nuilewis <lewisnuikweh@gmail.com>
2024-08-11 13:42:40 +0100

    Updated GradientImageBackground with video

--- app/src/main/java/com/example/craite/ui/screens/composables/GradientImageBackground.kt
+++ app/src/main/java/com/example/craite/ui/screens/composables/GradientImageBackground.kt
@@ -1,35 +1,117 @@
 package com.example.craite.ui.screens.composables
 
+import android.net.Uri
+import androidx.annotation.OptIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import com.example.craite.R
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
 import com.example.craite.ui.theme.AppColor
 
 
 @Composable
-fun GradientImageBackground(modifier: Modifier = Modifier, painter: Painter, contentDescription: String?, gradientColor: Color = AppColor().black) {
-    Box(modifier = Modifier.then(modifier)){
+fun GradientImageBackground(
+    modifier: Modifier = Modifier,
+    painter: Painter? = null,
+    contentDescription: String?,
+    gradientColor: Color = AppColor().black,
+    videoUri: Uri? = null,
+) {
+    Box(modifier = Modifier.then(modifier)) {
 
-    Image(
-        modifier = Modifier.fillMaxSize(),
-        painter = painter,
-        contentDescription = contentDescription,
-        contentScale = ContentScale.Crop
-    )
+        if (videoUri != null) {
+            VideoPlayer(videoUri = videoUri)
+        }
 
-    Box(modifier = Modifier.fillMaxSize().background(brush = Brush.verticalGradient(colors = listOf(
-        gradientColor.copy(alpha = 0f),
-        gradientColor
-    ),),),)
+        if (painter != null) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
 
+        }
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            gradientColor.copy(alpha = 0f),
+                            gradientColor
+                        ),
+                    ),
+                ),
+        )
+
+    }
 }
+
+@OptIn(UnstableApi::class)
+@Composable
+fun VideoPlayer(videoUri: Uri) {
+    val context = LocalContext.current
+
+    val videoPlayer = remember {
+        //Old implementation with VideoView
+//        VideoView(context).apply {
+//            setVideoURI(videoUri)
+//            setOnPreparedListener { mediaPlayer ->
+//                mediaPlayer.isLooping = true
+//                mediaPlayer.setVolume(0f, 0f)
+//                start()
+//            }
+//            start()
+//        }
+
+        ExoPlayer.Builder(context).build().apply {
+            val mediaItem = MediaItem.fromUri(videoUri)
+            setMediaItem(mediaItem)
+            repeatMode = Player.REPEAT_MODE_ALL
+            playWhenReady = true
+            videoScalingMode = VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+            prepare()
+        }
+    }
+
+    DisposableEffect(
+
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { context ->
+                PlayerView(context).apply {
+                    player = videoPlayer
+                    useController = false
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
+
+
+                }
+            }
+
+        )) {
+        onDispose { videoPlayer.release() }
+
+    }
+
+
 }
